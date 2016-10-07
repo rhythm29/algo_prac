4f82e8ee7c1a083aafb75b15ecb0fe7a23b6a16a
rhythmwaliamodi <rhythmwalia29@gmail.com>
2016-10-07 17:35:02 +0530

    WIP

--- src/algo/quick_sort.rb
+++ src/algo/quick_sort.rb
@@ -1,2 +1,20 @@
 class Algo::QuickSort
+	def doPartition (resultSoFar, item)
+		if item < pivotal
+			return [item].concat(resultSoFar)
+        else
+        	return resultSoFar.concat[item]
+        end
+	end
+
+	def partition(list, pivotal)
+		initialResult = [pivotal]
+		list.reduce(initialResult) do |resultSoFar, item|
+			if item < pivotal
+				return [item].concat(resultSoFar)
+	        else
+	        	return resultSoFar.concat[item]
+	        end	
+		end
+    end
 end
--- test/algo_spec.rb
+++ test/algo_spec.rb
@@ -1,10 +1,9 @@
 require "./test/spec_helper"
 describe Algo::QuickSort do
-
-  describe '#quicksort' do
-    it 'it should return bar when given foo' do
-      expect(1).to eq(1)
-    end
-
+	describe '#partition' do
+        it 'should return the same array back when the array has only one element' do
+        	qs = Algo::QuickSort.new
+           expect(qs.partition([1], 1)).to eq([1])
+        end
     end
 end
